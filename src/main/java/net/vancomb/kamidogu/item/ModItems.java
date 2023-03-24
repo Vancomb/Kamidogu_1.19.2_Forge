0ab6d08ed289b1281f77dd7b8fd88898fa854292
Vancomb <Vancomb97@gmail.com>
2023-03-24 18:32:57 -0400

    Drakesword added!

--- src/main/java/net/vancomb/kamidogu/item/ModItems.java
+++ src/main/java/net/vancomb/kamidogu/item/ModItems.java
@@ -2,6 +2,10 @@ package net.vancomb.kamidogu.item;
 
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,8 +21,23 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeTab.KAMIDOGU_TAB)));
 
+    //Sword
+    public static final RegistryObject<SwordItem> DRAKESWORD = ITEMS.register("drakesword",
+            () -> new SwordItem(Tiers.EXAMPLE, 0, 0, new Item.Properties().tab(ModCreativeTab.KAMIDOGU_TAB)));
+
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+    }
+
+    public static class Tiers {
+        public static final Tier EXAMPLE = new ForgeTier(
+                4,
+                800,
+                1.5f,
+                4,
+                350,
+                null, () -> Ingredient.EMPTY);
     }
 
 }
